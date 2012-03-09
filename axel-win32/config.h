7a03418f62098bdcabf02020b6b57bae07bdd7b7
xiangzhai83 <xiangzhai83@gmail.com>
2012-03-09 14:17:55 +0000

    code clean.

--- axel-win32/config.h
+++ axel-win32/config.h
@@ -4,6 +4,5 @@
 #define ETCDIR "/usr/local/etc"
 #define LOCALE "/usr/local/share/locale"
 #define ARCH "WIN32"
-/* FIXME: number of connection is 10, download big file freezed */
 #define NUM_CONN 10
 #define DEBUG
