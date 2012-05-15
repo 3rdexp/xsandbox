6cf0cfb64c52f96d7cbef324d766cc9a03ef286d
xiangzhai83@gmail.com <xiangzhai83@gmail.com@8a39c34e-998d-b01e-01e2-cfdea76ed7f2>
2012-05-15 03:41:13 +0000

    Add miniblogsync.

---
+++ trunk/miniblogsync/src/com/xzhai/MiniBlogSyncActivity.java
@@ -0,0 +1,13 @@
+package com.xzhai;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MiniBlogSyncActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
