6c3514cdb941861b71e1a47cef21e20da4a40b43
xiangzhai83 <xiangzhai83@gmail.com>
2012-05-23 08:53:54 +0000

    GetStatus hacking.

--- helloqq/src/HelloQQ.cpp
+++ helloqq/src/HelloQQ.cpp
@@ -2,33 +2,56 @@
 #include <tchar.h>
 #include <stdio.h>
 
+#include "mhook-lib/mhook.h"
+
+#define BUFFER_SIZE 1024
+
 static char* m_DllName = "HelloQQ";
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL,  // handle to DLL module
-					DWORD fdwReason,     // reason for calling function
-					LPVOID lpReserved)  // reserved
-{
-	FILE* fptr = NULL;
+static FILE* m_fptr = NULL;
 
+typedef int (WINAPI* _GetStatus)(ULONG);
+
+_GetStatus TrueGetStatus = (_GetStatus)
+	GetProcAddress(GetModuleHandle(_T("KernelUtil")), "GetStatus");
+
+int WINAPI HookGetStatus(ULONG QQUIN)
+{
+	char buffer[BUFFER_SIZE] = {'\0'};
+
+	// TODO: Hook GetStatus
+	sprintf(buffer, "QQUIN: %d", QQUIN);
+	if (m_fptr) 
+	{
+		fwrite(buffer, sizeof(char), sizeof(char) * strlen(buffer), m_fptr);
+		fflush(m_fptr);
+	}
+
+	return TrueGetStatus(QQUIN);
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,		// handle to DLL module
+					DWORD fdwReason,		// reason for calling function
+					LPVOID lpReserved)		// reserved
+{
     // Perform actions based on the reason for calling.
     switch (fdwReason) 
     { 
         case DLL_PROCESS_ATTACH:
          // Initialize once for each new process.
          // Return FALSE to fail DLL load.
-			fptr = fopen("C:\\test.txt", "w");
-			if (fptr == NULL) return TRUE;
-			fwrite(m_DllName, sizeof(char), sizeof(char) * strlen(m_DllName), fptr);
-			fflush(fptr);
+			m_fptr = fopen("C:\\HelloQQ.txt", "a");
+			Mhook_SetHook((PVOID*)&TrueGetStatus, HookGetStatus);
             break;
 
         case DLL_PROCESS_DETACH:
          // Perform any necessary cleanup.
-			if (fptr) 
+			if (m_fptr) 
 			{
-				fclose(fptr);
-				fptr = NULL;
+				fclose(m_fptr);
+				m_fptr = NULL;
 			}
+			Mhook_Unhook((PVOID*)&TrueGetStatus);
             break;
     }
     return TRUE;  // Successful DLL_PROCESS_ATTACH.
